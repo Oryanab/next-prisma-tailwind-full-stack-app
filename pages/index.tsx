857fb7dc7314bc943847c132ba1aa8938abda656
Oryanab <86202122+Oryanab@users.noreply.github.com>
2022-03-21 17:57:10 +0200

    setup tailwind & next js

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-red-200 font-bold">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Home;
